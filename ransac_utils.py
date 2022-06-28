08a0f85c01b2b415b70387e4044763f4d4daa4a5
Tahreem Rasul <rasul.tahreem1@gmail.com>
2022-06-28 11:12:41 +0500

    added mask calculation

--- ransac_utils.py
+++ ransac_utils.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 import numpy as np
 import numpy.linalg as la
+from scipy import signal, ndimage
 
-# RANSAC Helper functions
+# RANSAC HELPER FUNCTIONS
 def valid_cloud(img_reshaped):
     return img_reshaped[np.where(img_reshaped[:,2]!=0)]
 
@@ -37,3 +38,28 @@ def find_inliers(img_reshaped, model, threshold):
     num_inliers = np.count_nonzero(inliers == True)
 
     return num_inliers
+
+## MASK CALCULATION
+def get_plane_mask(img, model, threshold):
+    img_reshaped = img.reshape((-1, 3))
+    #distances = model[0] * img_reshaped[:, 0] + model[1] * img_reshaped[:, 1] + model[2] * img_reshaped[:, 2] - model[3]
+    ones = np.expand_dims(np.ones(img_reshaped.shape[0]), axis=1)
+    img_reshaped = np.hstack((img_reshaped, ones))
+
+    distances = np.abs(img_reshaped @ model) / np.sqrt(model[0] ** 2 + model[1] ** 2 + model[2] ** 2)
+    mask = np.where(distances < threshold, 1, 0)
+
+    return mask.reshape((img.shape[0], img.shape[1]))
+
+## OPENING AND CLOSING
+def opening(mask, num_neighbors=None):
+    if num_neighbors == None:
+        return ndimage.binary_opening(mask).astype(np.int)
+    else:
+        return ndimage.binary_opening(mask, structure=np.ones((num_neighbors, num_neighbors))).astype(np.int)
+
+def closing(mask, num_neighbors):
+    if num_neighbors == None:
+        return ndimage.binary_closing(mask).astype(np.int)
+    else:
+        return ndimage.binary_closing(mask, structure=np.ones((num_neighbors, num_neighbors))).astype(np.int)
